6567c70a8d5bd05f9b5b98d3ba26b1762f7812b1
manNomi <hanmw110@naver.com>
2026-03-22 02:59:17 +0900

    chore: setup tailwind and migrate homepage styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +11,13 @@ export default defineConfig({
   adapter: vercel({
     runtime: "nodejs20",
   }),
-  integrations: [react(), sitemap()],
+  integrations: [
+    react(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   markdown: {
     shikiConfig: {
       theme: "github-dark",
